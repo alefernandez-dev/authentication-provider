c284068590335e71fa6eced930a1a8eb9afda95a
alejandro <cesar.alejandro.j01@gmail.com>
2024-08-26 12:05:14 -0300

    disable session and add role hierarchy configuration

--- src/main/java/alexdev/authenticationprovider/auth/SecurityConfig.java
+++ src/main/java/alexdev/authenticationprovider/auth/SecurityConfig.java
@@ -4,11 +4,15 @@ import alexdev.authenticationprovider.user.Users;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,14 +43,23 @@ public class SecurityConfig {
 
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .requestCache(RequestCacheConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authenticationProvider(customAuthenticationProvider)
                 .authorizeHttpRequests(
                         c -> c
                                 .requestMatchers("/health").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/user").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.GET, "/user").hasRole("USER")
                                 .requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
                                 .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withDefaultRolePrefix()
+                .role("ADMIN").implies("USER")
                 .build();
     }
 
