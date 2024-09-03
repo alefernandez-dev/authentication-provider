1c17e33c7edd5376c25671888edf1ffc535bff48
alejandro <cesar.alejandro.j01@gmail.com>
2024-09-03 12:34:13 -0300

    fix code

--- src/main/java/alexdev/authenticationprovider/auth/CustomAuthenticationProvider.java
+++ src/main/java/alexdev/authenticationprovider/auth/CustomAuthenticationProvider.java
@@ -35,6 +35,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
